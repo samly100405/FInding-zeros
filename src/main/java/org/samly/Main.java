1fc210a4e9fe252038910613fef5f5813497cfbe
Sam Ly <sam@samly.me>
2024-10-04 12:41:03 -0700

    implement generatePolynomial

--- src/main/java/org/samly/Main.java
+++ src/main/java/org/samly/Main.java
@@ -3,10 +3,23 @@ package org.samly;
 import com.beust.jcommander.JCommander;
 import org.samly.cli.Args;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
+    private static Polynomial generatePolynomial(File file) throws FileNotFoundException {
+        Scanner s = new Scanner(file);
+
+        int degree = s.nextInt();
+
+        float[] coefficients = new float[degree + 1];
+        for (int i = 0; i < coefficients.length; i++) {
+            coefficients[i] = s.nextFloat();
+        }
+
+        return new Polynomial(degree, coefficients);
+    }
     public static void main(String[] args) throws FileNotFoundException {
         // generate polynomial from file
 
@@ -18,16 +31,7 @@ public class Main {
 
         System.out.println(a.getDescription());
 
-        Scanner s = new Scanner(a.getFile());
-
-        int degree = s.nextInt();
-
-        float[] coefficients = new float[degree + 1];
-        for (int i = 0; i < coefficients.length; i++) {
-            coefficients[i] = s.nextFloat();
-        }
-
-        Polynomial f = new Polynomial(degree, coefficients);
+        Polynomial f = generatePolynomial(a.getFile());
 
         for (int i = 0; i < 4; i++) {
             System.out.println("f(" + i + ") = " + f.evaluate(i));
